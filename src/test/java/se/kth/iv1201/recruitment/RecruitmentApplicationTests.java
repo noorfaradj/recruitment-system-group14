3fe8bf0cc13120cf29ea37aaefa610c87d167883
Noor <noorfaradj@outlook.com>
2026-01-22 11:45:48 +0100

    Add initial Spring Boot project

---
+++ src/test/java/se/kth/iv1201/recruitment/RecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
