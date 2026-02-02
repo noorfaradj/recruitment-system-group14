1bc5fa23251d5ce142b8dbb82aa8fcb871e92fdb
Noor <noorfaradj@outlook.com>
2026-02-02 13:14:01 +0100

    Iteration 2: Add service layer for job applications

--- src/main/java/se/kth/iv1201/recruitment/service/JobApplicationService.java
+++ src/main/java/se/kth/iv1201/recruitment/service/JobApplicationService.java
@@ -1,7 +1,18 @@
 package se.kth.iv1201.recruitment.service;
 
+import se.kth.iv1201.recruitment.dto.ApplicationListItemDTO;
+
+import java.util.List;
+
 /**
  * Business operations for job applications.
  */
 public interface JobApplicationService {
+
+    /**
+     * Lists all job applications for recruiter view.
+     *
+     * @return applications prepared for presentation
+     */
+    List<ApplicationListItemDTO> listAllApplications();
 }
